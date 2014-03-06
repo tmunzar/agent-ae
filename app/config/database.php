69cef03ecb4c360ccfa36512d2066c1420b04494
Talal Munzar <tmunzar@gmail.com>
2014-03-06 23:40:46 +0500

    please please work

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => $WERCKER_MYSQL_HOST,
-			'database'  => $WERCKER_MYSQL_DATABASE,
-			'username'  => $WERCKER_MYSQL_USERNAME,
-			'password'  => $WERCKER_MYSQL_PASSWORD,
+			'host'      => getenv('WERCKER_MYSQL_HOST'),
+			'database'  => getenv('WERCKER_MYSQL_DATABASE'),
+			'username'  => getenv('WERCKER_MYSQL_USERNAME'),
+			'password'  => getenv('WERCKER_MYSQL_PASSWORD'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
