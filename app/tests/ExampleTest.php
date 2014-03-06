30ade1d0b7af3e510d26844bc3229f4ac82e5565
Talal Munzar <tmunzar@gmail.com>
2014-03-06 21:51:36 +0500

    Test changed

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -9,9 +9,9 @@ class ExampleTest extends TestCase {
 	 */
 	public function testBasicExample()
 	{
-		$crawler = $this->client->request('GET', '/');
+		$this->call('GET', '/');
 
-		$this->assertTrue($this->client->getResponse()->isOk());
+    	$this->assertResponseOk();
 	}
 
 }
